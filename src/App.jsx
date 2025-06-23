ec7ca70529901120ec72c212e54f1359ab6874aa
Nikita Goncharov <nikitagoncharov993@gmail.com>
2025-06-23 01:50:58 -0400

    created data object

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import Home from './pages/Home';
 import PhotoshopProjects from './pages/PhotoshopProjects';
 import IllustratorProjects from './pages/IllustratorProjects';
+import DetailedPic from './pages/detailedPic';
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -10,7 +11,8 @@ function App() {
       <Route path='/' element={<Home/>}/>
       <Route path='photoshop' element={<PhotoshopProjects/>}/>
       <Route path='illustrator' element={<IllustratorProjects/>}/>
-
+      <Route path='photoshop/:id' element = {<DetailedPic/>}/>
+      <Route path='illustrator/:id' element = {<DetailedPic/>}/>
     </Routes>
   )
 
---
+++ src/assets/data.js
@@ -0,0 +1,144 @@
+import VotApp from './images/Ps/VotApp.png'
+import FinalProject from './images/Ps/FinalProject.png'
+import CuteVader from './images/Ps/project.png'
+import Sneakers from './images/Ps/project.png'
+import Zoobrid from './images/Ps/zoobrid.png'
+
+import Baymax from './images/AI/baymax.svg'
+import BeakAndMic from './images/AI/beakAndMic.svg'
+import Calligram from './images/AI/calligram.svg'
+import Future from './images/AI/FinalProject.svg'
+import flowershopLogo from './images/AI/flowershopLogo.svg'
+import fuguFish from './images/AI/fuguFish.svg'
+import GoGoose from './images/AI/GOGOOSE.svg'
+import gullInTheGrass from './images/AI/gullInTheGrass.svg'
+import gullOnWays from './images/AI/gullOnWays.svg'
+import gullWithABottle from './images/AI/gullWithABottle.svg'
+import Lyrics from './images/AI/lyrics.svg'
+import Shoe from './images/AI/shoe.svg'
+import ShyGull from './images/AI/shyGull.svg'
+import siteDesign from './images/AI/siteDesign.svg'
+
+
+const data = {
+    photoshop:[
+        {
+            id: 'votapp',
+            title: 'VotApp',
+            description: 'icon of non-existent voting app',
+            image: VotApp,
+        },
+        {
+            id: 'miraclelandscape',
+            title: 'Miracle landscape',
+            description: 'A magical landscape with cosmic theme',
+            image: FinalProject,
+        },
+         {
+            id: 'cutevader',
+            title: 'Cute Vader',
+            description: 'Cute Vader was made as a holliday card',
+            image: CuteVader,
+        },
+        {
+            id: 'zoobrid',
+            title: 'Zoobrid',
+            description: 'advertising poster for a fictional zoo with hybrids',
+            image: Zoobrid,
+        },
+        {
+            id: 'sneakers',
+            title: 'Sneakers ',
+            description: 'Design of a line of sneakers for a fictional brand',
+            image: Sneakers,
+        },
+        
+    ],
+    illustrator:[
+        {
+            id: 'baymax',
+            title: 'Baymax',
+            description: 'An svg picture of robot "Baymax" from big hero 6',
+            image: Baymax ,
+        },
+        {
+            id: 'beakandmic',
+            title: 'Beak and Mic',
+            description: 'Cover of a podcast about a seagull interviewer who interviews cartoon characters',
+            image: BeakAndMic,
+        },
+        {
+            id: 'caligram',
+            title: 'Caligram',
+            description: 'Caligram with space theme',
+            image: Calligram,
+        },
+        {
+            id: 'future',
+            title: 'Poster about future',
+            description: 'Poster that answer the question "What will future look like?"',
+            image: Future,
+        },
+        {
+            id: 'flowershoplogo',
+            title: 'Flower shop logo',
+            description: 'logo of a non-existent flower shop',
+            image: flowershopLogo,
+        },
+        {
+            id: 'fugufish',
+            title: 'Fugu fish',
+            description: 'Svg drawing of fugu fish',
+            image: fuguFish,
+        },
+        {
+            id: 'gogoose',
+            title: 'Go-goose',
+            description: 'Drawing of a superhero goose nicknamed "Go-goose"',
+            image: GoGoose,
+        },
+        {
+            id: 'gullinthegrass',
+            title: 'Gull in the grass',
+            description: 'Svg drawing of a seagull in the grass',
+            image: gullInTheGrass,
+        },
+        {
+            id: 'gullonways',
+            title: 'Gull on ways',
+            description: 'Svg drawing of a seagull on the waves',
+            image: gullOnWays,
+        },
+        {
+            id: 'gullwithabottle',
+            title: 'Gull with a bottle',
+            description: 'Svg drawing of a seagull with a bottle in its wings',
+            image: gullWithABottle,
+        },
+        {
+            id: 'lyrics',
+            title: 'Song lyrics',
+            description: 'fragment of lyrics from the "Just the two of us"',
+            image: Lyrics,
+        },
+        {
+            id: 'shoe',
+            title: 'Sneaker',
+            description: 'svg sneaker drawing',
+            image: Shoe,
+        },
+        {
+            id: 'shygull',
+            title: 'Shy gull',
+            description: 'Svg drawing of a shy seagull',
+            image: ShyGull,
+        },
+        {
+            id: 'sitedesign',
+            title: 'Web site design',
+            description: 'svg design of one of the pages of the google site',
+            image: siteDesign ,
+        },
+    ]
+}
+export default data 
